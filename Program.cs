a398dc47d2952923acebede7df8a4648ff35d817
Aliblack <alibekept@mail.ru>
2023-07-12 23:05:16 +0600

    MinMax

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿ Console.WriteLine ("Input first number");
+  int firstNum = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("Input second number");
+  int secNum = Convert.ToInt32(Console.ReadLine());
+  if (secNum > firstNum)
+  {
+
+    Console.WriteLine ($"{firstNum} > {secNum}");
+
+  }
+else 
+{
+
+    Console.WriteLine ($"{firstNum} > {secNum}");
+}
+
